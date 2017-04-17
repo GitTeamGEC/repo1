537dd5cc38c908fed633fd2b1f52033e8c04139d
Biswas B <Biswasb007@gmail.com>
2017-04-17 15:01:45 +0530

    Create logrotation.sh

---
+++ logrotation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# -eq 2 ]
+then
+	
+	filename=$1		# First argument is saved to fn
+	input=$2		# Second argument is saved to input
+	if [ -f $filename ]
+	then
+		echo "$2" >> $filename
+		echo "$(tail $filename)" > $filename
+		cat $filename
+	else
+		echo "File does not exist"
+	fi
+fi
